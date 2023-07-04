a6e0ca1d4a2fe28c7978343675fcb844dfc14a3b
Casey <caseyculbertson1@gmail.com>
2023-07-04 03:23:15 -0700

    kinda works

---
+++ python/drawing.py
@@ -0,0 +1,63 @@
+import pygame, sys
+
+tileSize = 25
+WHITE = pygame.Color("white")
+BLUE = pygame.Color("blue")
+GREY = pygame.Color("grey60")
+BLACK = pygame.Color("black")
+RED = pygame.Color("red")
+CYAN = pygame.Color("cyan4")
+PINK = pygame.Color("fuchsia")
+DARK_GREEN = pygame.Color("darkgreen")
+DARK_BLUE = pygame.Color("darkblue")
+DARK_RED = pygame.Color("darkred")
+LIGHT_GREY = pygame.Color("grey85")
+DARK_GREY = pygame.Color("grey25")
+
+num_colors = [BLUE, DARK_GREEN, RED, DARK_BLUE, DARK_RED, CYAN, BLACK, DARK_GREY, PINK]
+
+pygame.font.init()
+font = pygame.font.SysFont('Comic Sans MS', 20)
+
+empty_tile = pygame.Surface((tileSize,tileSize))
+empty_tile.fill(LIGHT_GREY)
+pygame.draw.rect(empty_tile,BLACK,empty_tile.get_rect(),1)
+
+covered_tile = pygame.Surface((tileSize,tileSize))
+covered_tile.fill(GREY)
+pygame.draw.rect(covered_tile,DARK_GREY,covered_tile.get_rect(),2)
+pygame.draw.rect(covered_tile,BLACK,covered_tile.get_rect(),1)
+
+flagged_tile = pygame.Surface.copy(covered_tile)
+pygame.draw.polygon(flagged_tile, RED, [(8,5),(17,9),(8,13)])
+pygame.draw.line(flagged_tile, BLACK, (8,5), (8,20))
+
+n_tiles = [empty_tile]
+
+for i in range(9):
+    n_surf = font.render(str(i+1), True, num_colors[i])
+    tile_surf = pygame.Surface.copy(empty_tile)
+    tile_surf.blit(n_surf,((tileSize-n_surf.get_width())//2, (tileSize-2-n_surf.get_height())//2))
+    n_tiles.append(tile_surf)
+
+if __name__ == "__main__":
+    root = pygame.display.set_mode((500,100))
+    
+    root.fill(WHITE)
+
+    x = 10
+
+    root.blit(covered_tile, (x,0))
+    x += 30
+
+    root.blit(flagged_tile, (x,0))
+    x += 30
+
+    for tile in n_tiles:
+        root.blit(tile, (x,0))
+        x += 30
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+        pygame.display.update()
--- python/main.py
+++ python/main.py
@@ -1,10 +1,15 @@
-import pygame, sys
+import pygame, sys, random
+from drawing import *
 pygame.init()
 
-GREY = (200,200,200)
-DARK_GREY = (150,150,150)
-BLACK = (0,0,0)
+neighbors = [(-1,-1),(-1,0),(-1,1),(0,1),(1,1),(1,0),(1,-1),(0,-1)]
 
+class Tile:
+    mine = False
+    covered = False
+    value = 0
+    flag = False
+    
 argc = len(sys.argv)
 
 winW = 1000
@@ -12,7 +17,7 @@ winH = 1000
 fieldW = 20
 fieldH = 15
 density = 10
-tileSize = 25
+
 
 if (argc > 1): fieldW = int(sys.argv[1])
 if (argc > 2): fieldH = int(sys.argv[2])
@@ -21,9 +26,10 @@ if (argc > 3): density = int(sys.argv[3])
 fieldX = (winW - tileSize*fieldW)//2
 fieldY = 100
 
-root = pygame.display.set_mode((winW,winH))
+field = []
+for x in range(fieldW): field.append([None]*fieldH)
 
-r1 = pygame.Rect(20,10,100,200)
+root = pygame.display.set_mode((winW,winH))
 
 pygame.font.init()
 font = pygame.font.SysFont('Comic Sans MS', 20)
@@ -31,24 +37,71 @@ font = pygame.font.SysFont('Comic Sans MS', 20)
 
 clock = pygame.time.Clock()
 
-def draw_tile(tile_rect):
-    pygame.draw.rect(root, GREY, tile_rect)
-    pygame.draw.rect(root, DARK_GREY, tile_rect, 3)
-    pygame.draw.rect(root, BLACK, tile_rect, 1)
+def draw_tile(x, y, tile):
+    if tile.covered:
+        if tile.flag: surf = flagged_tile
+        else: surf = covered_tile
+    else:
+        surf = n_tiles[tile.value]
+    
+    root.blit(surf,(x,y))
+    
+def get_tile(x,y):
+    if 0 <= x < fieldW and 0 <= y < fieldH:
+        return field[x][y]
+    return None
+
+def gen_field():
+    for y in range(fieldH):
+        for x in range(fieldW):
+            tile = Tile()
+            tile.mine = density > random.randint(0,99)
+            tile.covered = True
+            field[x][y] = tile
+
+def mark(x,y):
+    tile = get_tile(x,y)
+    if not tile: return
+    tile.flag ^= 1
+
+def reveal(x,y):
+    tile = get_tile(x,y)
+    if not (tile and tile.covered): return
+
+    tile.covered = False
+
+    if tile.mine:
+        print("YOU LOST")
+        exit()
+
+    for xo,yo in neighbors:
+        x1,y1 = x+xo,y+yo
+        neigh = get_tile(x1,y1)
+        if neigh:
+            tile.value += neigh.mine
+            if tile.value == 0: reveal(x1,y1)
+    
+gen_field()
 
 while True:
     dt = clock.tick(60)/1000
+    mouse = pygame.mouse.get_pos()
+    selectX = (mouse[0]-fieldX)//tileSize
+    selectY = (mouse[1]-fieldY)//tileSize
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
         if event.type == pygame.MOUSEBUTTONDOWN:
-            pass
-
+            if event.button == 1:
+                reveal(selectX,selectY)
+            elif event.button == 3:
+                mark(selectX,selectY)
+    
     root.fill((255,255,255))
     for x in range(fieldW):
         for y in range(fieldH):
-            tile = pygame.Rect(x*tileSize+fieldX,y*tileSize+fieldY,tileSize,tileSize)
-            draw_tile(tile)
+            xp,yp = x*tileSize+fieldX, y*tileSize+fieldY
+            draw_tile(xp,yp,field[x][y])
             
     #root.blit(text,(200,100))
     pygame.display.update()
